d52ce3ad9d1c0c32edb5880f97d9ac7ea0e0669a
Muhammad Ali <alimunir1445@gmail.com>
2025-10-21 19:28:06 +0500

    Connect the mongoDb

---
+++ src/app/lib/dbConnect.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+async function dbConnect() {
+  if (connection.isConnected) {
+    console.log("Db already Connected");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB || "");
+    console.log("Log Db", db);
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log("Log connections", db.connections);
+    console.log("DB connected SuccessFullly");
+  } catch (error) {
+    console.log("Db connection Failed", error);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
